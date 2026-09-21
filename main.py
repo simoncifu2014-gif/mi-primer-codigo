0db494375b555d2262bff3363fcca64db5e8953d
simoncifu2014-gif <simoncifu2014@gmail.com>
2026-09-21 20:12:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print("meme_dict[word")
+else:
+    print("esa palabra no esta definida")
